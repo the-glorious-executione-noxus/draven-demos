d4c964a7bb47dfc9d95825f43ee0cc06d32427d2
1064517852@qq.com <w1994z0115x>
2020-02-10 16:04:56 +0800

    k8s部署eurekademo事例

---
+++ noxus-draven-k8s-demos/src/main/java/com/noxus/draven/k8s/EurekaApplication.java
@@ -0,0 +1,26 @@
+package com.noxus.draven.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
+@RestController
+@RefreshScope
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+
+    public String testConfig() {
+
+        return "testConfig";
+    }
+}
